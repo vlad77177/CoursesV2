3a4cecf6cd30237ba299b37e04a4e22ee3df7d49
vkosobok <vkosobok@mera.ru>
2018-08-09 10:14:47 +0300

    create branch

---
+++ public_html/php/classes/SQLExec.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class SQLExec{
+    
+    private $connection;
+    
+    function Connect($host,$user,$password,$database){
+        $this->connection=mysqli_connect($host,$user,$password,$database);
+    }
+    
+    function ExecuteQuery($sql){
+        $result=array();
+        $res=mysqli_query($this->connection,$sql);
+        if($res!==null){
+            while($r=mysqli_fetch_assoc($res)){
+                $result[count($result)]=$r;
+            }
+        }
+        return $result;
+    }
+}
+
+?>
+
