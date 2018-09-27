f4341764314a558861469b7db82c8c4e1f22d746
vkosobok <vkosobok@mera.ru>
2018-09-27 16:57:58 +0300

    add db.php to gitignore

--- public_html/php/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db= mysqli_connect('mysql.db', 'kibertest', 'qwe*asd', 'kibertest') or die('Ошибка подключения к базе данных!');
-    mysqli_set_charset($db,"utf8");
-    date_default_timezone_set('Europe/Moscow');
-?>
-
